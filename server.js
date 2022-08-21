724d3fdc25762d8aa7ff1fefeb78e1800d0de140
Minotaurius <brandonslanger@gmail.com>
2022-08-20 19:53:44 -0500

    Set up server

--- server.js
+++ server.js
@@ -5,7 +5,29 @@ const PORT = process.env.PORT || 3333;
 const db = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 require('dotenv').config();
-
+const { view_routes, auth_routes } = require('./controllers');
 const app = express();
+
+app.use(express.static(path.join('front')));
+app.engine('hbs', engine({ extname: '.hbs'}));
+app.set('view engine', 'hbs')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: process.env.SECRET,
+    store: new SequelizeStore({ db }),
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        // httpOnly: true
+    }
+}));
+
+app.use('/', view_routes);
+app.use('/auth', auth_routes);
+
+db.sync().then(() => {
+    app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+});
